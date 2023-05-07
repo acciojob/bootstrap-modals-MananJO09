227a8bcd0c23bfc00c48d5cf4698c01fb5605235
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-07 23:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your code here
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("open-modal");
+
+  // Get the modal
+  var modal = document.getElementById("accio-modal");
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
